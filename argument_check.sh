f9a3c9a37bc0410726042dd06d1c31351a47b292
brenoAmodesto <brenoamodesto58@gmail.com>
2022-01-24 19:29:44 -0300

    main

--- argument_check.sh
+++ argument_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-arg={$1}
-
-if [ $arg = "pizza" ]; then
-    echo "Com maça?"
-else
-    echo "Eu quero pizza"
-fi
+#arg={$1}
+#Não está funcionando...
+#if [ $arg = "pizza" ]; then
+#    echo "Com maça?"
+#else
+#    echo "Eu quero pizza"
+#fi
