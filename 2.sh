dbe1b0b5c86bea94c97a1bc6b3af497ad5050852
brenoAmodesto <brenoamodesto58@gmail.com>
2022-03-17 19:19:46 -0300

    main

--- 2.sh
+++ 2.sh
@@ -13,3 +13,12 @@ OT_CAT="$(cat /etc/passwd | grep illi)"
 echo "$SAIDA_CAT"
 echo " P 1: $1"
 echo " P 2: $2"
+
+#!/bin/bash
+
+var1=$1
+
+if [ $var1 -gt 10 ];
+then
+   echo "Nome $0 e o PID: $$"
+fi
